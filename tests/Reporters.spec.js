71c353989eb9ce6ab6b57bc5bad9b895dd8a7a6f
SvetlanaVdovenko <svetlanasavchenko85@gmail.com>
2024-01-05 13:33:35 +0200

    Lesson 35

---
+++ tests/Reporters.spec.js
@@ -0,0 +1,24 @@
+const {test, expect} = require('@playwright/test')
+
+test('Test 1', async ({page}) => {
+    await page.goto('https://demoblaze.com/index.html');
+    await expect(page).toHaveTitle('STORE');
+})
+
+test('Test 2', async ({page}) => {
+    await page.goto('https://demo.opencart.com/');
+    await expect(page).toHaveTitle('Your Store');
+})
+
+test('Test 3', async ({page}) => {
+    await page.goto('https://demo.nopcommerce.com/');
+    await expect(page).toHaveTitle('nopCommerce demo store');
+})
+
+// open configuration file and set the desired type of the reporter
+// html
+// list
+// line
+// dot
+// reporter: [['json', { outputFile: 'results.json' }]]
+// reporter: [['junit', { outputFile: 'results.xml' }]]
